9b91b291efa2e650522b3c98ded07aca0f6a1543
jnnkhffr <jannik.hoeffer@gmx.de>
2026-04-29 11:53:55 +0200

    correct calculation

--- Exercises/Sea_Ice_Temperature_Feedbacks.py
+++ Exercises/Sea_Ice_Temperature_Feedbacks.py
@@ -31,3 +31,49 @@ plt.legend()
 plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+
+#2.2
+
+H =0.5 #ice thickness
+Tbot = -1.8 + 273.15 #bottom Temperature in Kelvin
+K =2.2 #thermische conductivity of ice
+SIGMA = 5.67e-8 #Stefan Boltzmann constante'
+EPS = 0.95 #emissivity
+
+#arrays fore result
+days = np.arange(1,365)
+Ttop = np.zeros_like(days, dtype=float)
+
+#loop over all days
+for i, day in enumerate(days):
+    Fsw= shortwave(day)
+    Fother = otherfluxes(day)
+    alpha = albedo()
+
+    # Equation:
+    # -(1 - α) Fsw - Fother + ε σ T^4 = -k (T - Tbot)/h
+    #
+    # Rearranged into:
+    # εσ T^4 + (k/h) T + [ (1-α)Fsw + Fother - (k/h)Tbot ] = 0
+
+    a = EPS * SIGMA
+    b = 0.0
+    c = 0.0
+    d = K / H
+    e = (1 - alpha) * Fsw + Fother - (K / H) * Tbot
+
+    roots = np.roots([a,b,c,d,e])
+    print(roots)
+
+    # Select real roots
+    real_roots = roots[np.isreal(roots)].real
+
+    # Physical root = larger real root
+    Tphys = np.max(real_roots)
+
+    # Cap at melting point
+    if Tphys > 273.15:
+        Tphys = 273.15
+
+    Ttop[i] = Tphys
