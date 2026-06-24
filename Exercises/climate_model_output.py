8fb677dc0c74b1050c62f1cdc6e390d307ee6585
jnnkhffr <jannik.hoeffer@gmx.de>
2026-06-24 11:54:04 +0200

    adding the plotting

--- Exercises/climate_model_output.py
+++ Exercises/climate_model_output.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import matplotlib.pyplot as plt
 
 LAT_MIN = 66
 LAT_MAX = 90
@@ -9,3 +10,16 @@ file = "C:/Users/janni/Desktop/Studium/Master/Sea Ice/Exercises/sithick_SImon_MP
 
 df = xr.open_dataset(file)
 print(list(df.data_vars)) #['time_bnds', 'sithick']
+
+df_arctic = df.sel(lat=slice(LAT_MIN, LAT_MAX))
+sithick_arctic_mean = df_arctic["sithick"].mean(dim=["lat", "lon"])
+
+plt.figure(figsize=(10,5))
+plt.plot(df["time"], sithick_arctic_mean, label= "Sea ice thickness")
+
+plt.title("Arctic Sea Ice thickness")
+plt.xlabel("Years")
+plt.ylabel("Sea Ice Thickness (m)")
+plt.grid(True)
+plt.legend()
+plt.show()
